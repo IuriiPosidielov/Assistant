304eecc57d1105677f8678878e5f86516e4bdea9
IuriiPosidielov <posidelov@hotmail.com>
2023-04-11 14:20:16 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ chatgpt_key = ""
 
 hotword_keywords = ['bumblebee', 'ok google', 'computer', 'terminator']
 language = "ru"
+directoryPath = "/home/pi/apps/Assistant" 
 
 # youtube search context
 search_results = []
